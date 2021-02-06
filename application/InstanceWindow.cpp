d821e44d98d00e5ca8f2ceb7f0acb97730e2849e
Gh-raf <71462480+Gh-raf@users.noreply.github.com>
2021-02-06 20:00:45 +0000

    Changes from MultiMC5 (#1)
    * Switch Session Server
    
    sessionserver.mojang.com has been down for a while and it looks like it is no longer in use.
    https://bugs.mojang.com/browse/WEB-3166?focusedCommentId=820167&page=com.atlassian.jira.plugin.system.issuetabpanels%3Acomment-tabpanel#comment-820167
    
    I have switched the url to session.minecraft.net
    
    * Fix up 1.7.10 forge versions for Technic platform import by removing -1.7.10 suffix
    
    * NOISSUE set instance dialog launch/kill button object name
    
    * NOISSUE force kill button to reevaluate its style on state change
    
    * NOISSUE update changelog for 0.6.12
    
    * GH-3229 fix copy seed button not working for newer worlds
    
    Added the `optional-bare` library and refactored NBT reading
    code to support this change.
    
    * NOISSUE Remove proxy user and password from the MultiMC log
    
    Co-authored-by: TreyRuffy <TreyRuffy@users.noreply.github.com>
    Co-authored-by: Petr Mrázek <peterix@users.noreply.github.com>
    Co-authored-by: kb1000 <kaeptmblaubaer1000@gmail.com>
    Co-authored-by: Petr Mrázek <peterix@gmail.com>

--- application/InstanceWindow.cpp
+++ application/InstanceWindow.cpp
@@ -123,12 +123,14 @@ void InstanceWindow::updateLaunchButtons()
     {
         m_launchOfflineButton->setEnabled(false);
         m_killButton->setText(tr("Kill"));
+        m_killButton->setObjectName("killButton");
         m_killButton->setToolTip(tr("Kill the running instance"));
     }
     else if(!m_instance->canLaunch())
     {
         m_launchOfflineButton->setEnabled(false);
         m_killButton->setText(tr("Launch"));
+        m_killButton->setObjectName("launchButton");
         m_killButton->setToolTip(tr("Launch the instance"));
         m_killButton->setEnabled(false);
     }
@@ -136,8 +138,12 @@ void InstanceWindow::updateLaunchButtons()
     {
         m_launchOfflineButton->setEnabled(true);
         m_killButton->setText(tr("Launch"));
+        m_killButton->setObjectName("launchButton");
         m_killButton->setToolTip(tr("Launch the instance"));
     }
+    // NOTE: this is a hack to force the button to recalculate its style
+    m_killButton->setStyleSheet("/* */");
+    m_killButton->setStyleSheet(QString());
 }
 
 void InstanceWindow::on_btnLaunchMinecraftOffline_clicked()
